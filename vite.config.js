7e531036b2ab550a773ddf60a7d3f76a47d14c9a
Dmitry Litvinenko <ds-sev@ya.ru>
2023-09-21 22:40:56 +0300

    feat: add babel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import autoprefixer from 'autoprefixer'
+import babel from 'vite-plugin-babel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel(),
+  ],
   base: './',
   css: {
     postcss: {
