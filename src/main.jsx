67fe6511b55ca688788120a6ed9d4d98a58c4e70
Dmitry Litvinenko <ds-sev@ya.ru>
2023-09-22 20:29:22 +0300

    reinstall dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { store } from './store/configureStore'
 import App from './components/App/App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import 'promise-polyfill/src/polyfill';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import autoprefixer from 'autoprefixer'
-import legacy from '@vitejs/plugin-legacy'
-import 'core-js/actual';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime', 'core-js/modules/es.promise.js'],
-      polyfills: ['es.array.iterator']
-    })
+    // legacy({
+    //   targets: ['ie >= 11'],
+    //   additionalLegacyPolyfills: ['regenerator-runtime/runtime', 'core-js/modules/es.promise.js'],
+    //   polyfills: ['es.array.iterator']
+    // })
   ],
   base: './',
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer({}) // add options if needed
-      ]
-    }
-  }
 })
