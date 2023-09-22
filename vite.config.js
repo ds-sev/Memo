6848e50d70417810aaa046012ee83cc2fbb89050
Dmitry Litvinenko <ds-sev@ya.ru>
2023-09-22 22:35:51 +0300

    add plugin-legacy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    legacy({
+      targets: ['IE >= 11'],
+      additionalLegacyPolyfills: ['whatwg-fetch']
+    })
     // legacy({
     //   targets: ['ie >= 11'],
     //   additionalLegacyPolyfills: ['regenerator-runtime/runtime', 'core-js/modules/es.promise.js'],
