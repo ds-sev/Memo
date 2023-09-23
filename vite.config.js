9567348ec85e124584414d84c25a24403522bce8
Dmitry Litvinenko <ds-sev@ya.ru>
2023-09-23 10:43:49 +0300

    add polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
+    nodePolyfills(),
     legacy({
       targets: ['defaults', 'IE >= 10'],
       additionalLegacyPolyfills: ['whatwg-fetch', 'regenerator-runtime/runtime', 'core-js/modules/es.promise.js'],
-      polyfills: ['es.array.iterator']
+      polyfills: ['es.array.iterator', 'es/map', 'es/set', 'es/promise', 'es/array', 'es/function', 'es/math', 'es/object', 'es/symbol']
     })
   ],
   base: './'
