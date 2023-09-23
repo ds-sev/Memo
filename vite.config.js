83a99d4fa12b3be1b5667cea3c500bf3322666e2
Dmitry Litvinenko <ds-sev@ya.ru>
2023-09-23 03:23:47 +0300

    fix autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import autoprefixer from 'autoprefixer'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    legacy({
+      targets: ['defaults', 'IE >= 10'],
+      additionalLegacyPolyfills: ['whatwg-fetch']
+    }),
+  ],
   base: './',
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer({}) // add options if needed
-      ],
-    }
-  }
 })
