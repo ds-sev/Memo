cf93a0c3407c074c5ec605906ae728e007e84303
Dmitry Litvinenko <ds-sev@ya.ru>
2023-09-23 12:29:26 +0300

    add polyfills

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import 'promise-polyfill/src/polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { store } from './store/configureStore'
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(),
-    nodePolyfills(),
     legacy({
-      targets: ['defaults', 'IE >= 10'],
-      additionalLegacyPolyfills: ['whatwg-fetch', 'regenerator-runtime/runtime', 'core-js/modules/es.promise.js'],
-      polyfills: ['es.array.iterator', 'es/map', 'es/set', 'es/promise', 'es/array', 'es/function', 'es/math', 'es/object', 'es/symbol']
+      targets: ['defaults', 'ie >= 10']
     })
+    // legacy({
+    //   targets: ['defaults', 'IE >= 10'],
+    //   additionalLegacyPolyfills: ['whatwg-fetch', 'regenerator-runtime/runtime', 'core-js/modules/es.promise.js'],
+    //   polyfills: ['es.array.iterator', 'es/map', 'es/set', 'es/promise', 'es/array', 'es/function', 'es/math', 'es/object', 'es/symbol']
+    // })
   ],
-  base: './'
 })
